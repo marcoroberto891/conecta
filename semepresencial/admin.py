dfa7a9adb9f04906703f0a4fcb136e2abc78301f
marcoroberto891 <marcorjfaria@gmail.com>
2018-03-26 21:32:52 -0300

    3 commit via note

--- semepresencial/admin.py
+++ semepresencial/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from semepresencial.models import Pessoa, PessoaFisica, PessoaJuridica, Produto
 # Register your models here.
+
+admin.site.register(Pessoa)
+admin.site.register(PessoaFisica)
+admin.site.register(PessoaJuridica)
+admin.site.register(Produto)
--- semepresencial/models.py
+++ semepresencial/models.py
@@ -1,28 +1,57 @@
 from django.db import models
 
 
-class Pessoa():
-    nome = ''
-    data_nascimento = None
-
-    def __init__(self, nome, data_nascimento):
-        self.nome = nome
-        self.data_nascimento = data_nascimento
-
-        def __str__(self):
-            return self.nome
+class User(models.Model):
+    login = models.CharField('login',max_length=100)
+    senha = models.CharField('senha',max_length=6)
 
     ##HERANÇA
-class PessoaFisica(Pessoa):
-        def __init__(self, cpf):
-            self.cpf = cpf
-
-        def __str__(self):
-            return u'{} - {}'.format(self.cpf, self.nome)
-
-   ##ENCAPSULAMENTO
 
 
-    def __init__(self, nome, data_nascimento):
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=128)
+    email = models.CharField(max_length=150)
+    usuario = models.CharField(User, related_name='users_admin', null=True,blank=False)
+    ## self.endereco = Endereco('rua 1')  ##COMPOSICAO
+
+    def __init__(self, nome, email, usuario):  ##POLIMORFISMO
         self.nome = nome
-        self.data_nascimento = data_nascimento
+        self.email = email
+        self.usuario = usuario
+
+    def __str__(self):
+        return self.nome
+
+
+class PessoaFisica(Pessoa):
+    cpf = models.CharField(max_length=12)
+
+    def __init__(self, nome, email, usuario):  ##POLIMORFISMO
+        Pessoa.__init__(self, nome, email, usuario)
+
+
+    def __str__(self):
+        return self.cpf
+
+
+class PessoaJuridica(Pessoa):
+        cnpj = None
+
+        def __init__(self, nome, email, usuario, cnpj):  ##POLIMORFISMO
+            Pessoa.__init__(self, nome, email, usuario)
+            self.cnpj = cnpj
+
+
+    ##ENCAPSULAMENTO
+class Produto():
+    nome= ''
+    descricao= None
+    validade = None
+
+    def __init__(self, nome, descricao, validade):
+        self.nome = nome
+        self.descricao = descricao
+        self.validade = validade
+
+    def __str__(self):
+        return self.nome
